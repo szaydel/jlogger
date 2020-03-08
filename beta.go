ac71759bc2a4e2ae1ebbb8e4aafc71448028ba91
Sam Zaydel <szaydel@gmail.com>
2020-03-08 04:04:55 +0000

    Add murmur3 hash and improve duplicate handling

--- beta.go
+++ beta.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"hash"
-	//"fmt"
-	"hash/fnv"
+	"encoding/json"
 )
 
 type Message struct {
@@ -14,22 +12,35 @@ type Message struct {
 type CounterMap map[uint32]*Message
 
 type Messages struct {
-	h hash.Hash32
 	cm CounterMap
+	// mtx sync.
 }
 
 func NewMap() *Messages {
 	m := &Messages{
-		h: fnv.New32a(),
 		cm: make(CounterMap),
 	}
 	return m
 }
 
+func (m *Messages) sum(b []byte) uint32 {
+	mi := make(map[string]interface{})
+	if err := json.Unmarshal(b, &mi); err == nil {
+		// If this is valid JSON we should have a "msg" or "message"
+		// key in the map. If neither key exists, we take a sum of
+		// entire byte slice as a fallback mechanism.
+		if msg, ok := getValue("msg", mi); ok {
+			return SumString32(msg)
+		}
+		if msg, ok := getValue("message", mi); ok {
+			return SumString32(msg)
+		}
+	}
+	return Sum32(b)
+}
+
 func (m *Messages) Insert(b []byte) bool {
-	m.h.Reset()
-	m.h.Write(b)
-	sum := m.h.Sum32()
+	sum := m.sum(b)
 	if _, ok := m.cm[sum]; !ok {
 		m.cm[sum] = &Message{b, 1}
 		return true
