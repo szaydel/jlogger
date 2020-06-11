072e0d09bb73defe5103dc616968e785f8794282
Sam Zaydel <szaydel@gmail.com>
2020-06-11 04:16:49 +0000

    Expose stats via Prometheus

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"log/syslog"
 	"math"
@@ -394,33 +396,153 @@ func computeRatio(n, d int64) float64 {
 	return float64(n) / float64(d)
 }
 
+// Counters is meant to be private, but it is "exported" in order to be
+// JSON-marshal(able). In reality, it is not visible outside of this method.
+type Counters struct {
+	Line      int64 `json:"line"`
+	Plain     int64 `json:"plaintext"`
+	Json      int64 `json:"json_encoded"`
+	JsonNoMsg int64 `json:"json_encoded_missing_message"`
+	Duplicate int64 `json:"duplicate"`
+	Crit      int64 `json:"critical"`
+	Err       int64 `json:"error"`
+	Warning   int64 `json:"warning"`
+	Notice    int64 `json:"notice"`
+	Info      int64 `json:"info"`
+	Debug     int64 `json:"debug"`
+}
+
+type CounterDesc struct {
+	name        string
+	helpMsg     string
+	metricType  string
+	metricValue int64
+}
+
+func (c *Counters) WriteTo(w io.Writer) (int64, error) {
+	cdMap := map[stat]CounterDesc{
+		Crit: CounterDesc{
+			name:        "messages_level_crit_total",
+			helpMsg:     "Total number of messages logged with level CRITICAL",
+			metricType:  "counter",
+			metricValue: c.Crit,
+		},
+		Err: CounterDesc{
+			name:        "messages_level_err_total",
+			helpMsg:     "Total number of messages logged with level ERROR",
+			metricType:  "counter",
+			metricValue: c.Err,
+		},
+		Warning: CounterDesc{
+			name:        "messages_level_warn_total",
+			helpMsg:     "Total number of messages logged with level WARNING",
+			metricType:  "counter",
+			metricValue: c.Warning,
+		},
+		Notice: CounterDesc{
+			name:        "messages_level_notice_total",
+			helpMsg:     "Total number of messages logged with level NOTICE",
+			metricType:  "counter",
+			metricValue: c.Notice,
+		},
+		Info: CounterDesc{
+			name:        "messages_level_info_total",
+			helpMsg:     "Total number of messages logged with level INFO",
+			metricType:  "counter",
+			metricValue: c.Info,
+		},
+		Debug: CounterDesc{
+			name:        "messages_level_debug_total",
+			helpMsg:     "Total number of messages logged with level DEBUG",
+			metricType:  "counter",
+			metricValue: c.Debug,
+		},
+		Plain: CounterDesc{
+			name:        "plaintext_messages_total",
+			helpMsg:     "Total number of plaintext messages",
+			metricType:  "counter",
+			metricValue: c.Plain,
+		},
+		Json: CounterDesc{
+			name:        "json_encoded_messages_total",
+			helpMsg:     "Total number of JSON encoded messages",
+			metricType:  "counter",
+			metricValue: c.Json,
+		},
+		Duplicate: CounterDesc{
+			name:        "duplicate_messages_total",
+			helpMsg:     "Total number of messages recorded as duplicates",
+			metricType:  "counter",
+			metricValue: c.Duplicate,
+		},
+	}
+
+	return prometheusExpoWriter(w, cdMap)
+}
+
+func prometheusExpoWriter(
+	w io.Writer,
+	cdMap map[stat]CounterDesc) (int64, error) {
+	// Allocate a buffer and write all necessary lines to the buffer before
+	// sending the entire buffer to the supplied writer. This reduces the amount
+	// of accounting which we need to do here in terms of tracking bytes
+	// actually written. We just return result of a single WriteTo(...) with the
+	// supplied writer, after we fill the buffer with all required information.
+	var buf bytes.Buffer
+	for _, v := range []stat{
+		Crit,
+		Err,
+		Warning,
+		Notice,
+		Info,
+		Debug,
+		Plain,
+		Json,
+		Duplicate,
+	} {
+
+		if _, err := fmt.Fprintf(&buf, "# HELP %s %s\n",
+			cdMap[v].name, cdMap[v].helpMsg); err != nil {
+			return 0, err
+		}
+		if _, err := fmt.Fprintf(&buf, "# TYPE %s %s\n",
+			cdMap[v].name, cdMap[v].metricType); err != nil {
+			return 0, err
+		}
+		if _, err := fmt.Fprintf(&buf, "%s %d\n",
+			cdMap[v].name, cdMap[v].metricValue); err != nil {
+			return 0, err
+		}
+	}
+	return buf.WriteTo(w)
+}
+
 // stats tracks number of unique messages received. Levels are only extracted
 // from JSON messages where object contains field called "level", and that
 // field contains a name known to this utility.
-func (p *Publish) stats() {
+func (p *Publish) stats(w io.Writer) {
 	const reportInterval = 10
 
 	defer p.AckDone() // join the main goroutine
-	// Counters is meant to be private, but it is "exported" in order to be
-	// JSON-marshal(able). In reality, it is not visible outside of this method.
-	type Counters struct {
-		Line      int64 `json:"line"`
-		Plain     int64 `json:"plaintext"`
-		Json      int64 `json:"json_encoded"`
-		JsonNoMsg int64 `json:"json_encoded_missing_message"`
-		Duplicate int64 `json:"duplicate"`
-		Crit      int64 `json:"critical"`
-		Err       int64 `json:"error"`
-		Warning   int64 `json:"warning"`
-		Notice    int64 `json:"notice"`
-		Info      int64 `json:"info"`
-		Debug     int64 `json:"debug"`
-	}
 	var counts [Unknown]int64
 	var t = time.NewTicker(reportInterval * time.Second).C
 	for {
 		select {
 		case <-t:
+			c := &Counters{
+				Line:      counts[Line],
+				Plain:     counts[Plain],
+				JsonNoMsg: counts[JsonNoMsg],
+				Json:      counts[Json],
+				Duplicate: counts[Duplicate],
+				Crit:      counts[Crit],
+				Err:       counts[Err],
+				Warning:   counts[Warning],
+				Notice:    counts[Notice],
+				Info:      counts[Info],
+				Debug:     counts[Debug],
+			}
+			c.WriteTo(w)
 			b, _ := json.Marshal(
 				map[string]interface{}{
 					"counts": Counters{
@@ -943,14 +1065,14 @@ type topics struct {
 // by the dispatch function, which itself sits in a loop and scanning data from
 // stdin.
 type Publish struct {
-	conf        *args // arguments from command line
-	chans       *topics
-	ackDoneChan chan struct{}
-	doneChan    chan struct{}
-	statsChan   chan stat
-	source      string
+	conf         *args // arguments from command line
+	chans        *topics
+	ackDoneChan  chan struct{}
+	doneChan     chan struct{}
+	statsChan    chan stat
+	source       string
 	numOfWorkers int
-	transform   Transformer
+	transform    Transformer
 }
 
 func (p *Publish) AckDone() {
@@ -1133,6 +1255,33 @@ func (p *Publish) publishToRedis(ps PubSubInterface) {
 	}
 }
 
+type PrometheusExpoWriter struct {
+	fp string
+}
+
+// Write implements the writer interface and allows for implementing a customer
+// writer, which encapsulates writing to a file and additional file management
+// operations, such as creating a temporary file, etc.
+func (s *PrometheusExpoWriter) Write(data []byte) (n int, err error) {
+	var f *os.File
+	var permFilename = s.fp
+	var tmpFilename = permFilename + ".t"
+
+	if f, err = os.OpenFile(
+		tmpFilename,
+		os.O_RDWR|os.O_CREATE, 0644); err != nil {
+		return 0, err
+	} else {
+		defer func() {
+			f.Close()
+			if err = os.Rename(tmpFilename, permFilename); err != nil {
+				log.Panic(err)
+			}
+		}()
+	}
+	return f.Write(data)
+}
+
 func signalHandler(sig chan os.Signal, done chan struct{}) {
 	v := <-sig
 	log.Printf("Shutting down on %v signal", v)
@@ -1185,7 +1334,10 @@ func main() {
 	dupes := NewMap()
 
 	// Start statistics processing goroutine
-	go p.stats()
+	pw := &PrometheusExpoWriter{
+		fp: "/run/jlogger/stats.prom",
+	}
+	go p.stats(pw)
 	p.numOfWorkers += 1
 
 	// Start dispatch goroutine (it feeds the publishing goroutines)
